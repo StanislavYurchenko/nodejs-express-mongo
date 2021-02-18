01d05f2da62b7db00faf5e9894c607328f87a130
Yurchenko Stanislav <yurchenko.stanislav@ukr.net>
2021-02-18 12:13:46 +0200

    hw02 ready for check

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,76 @@
 const express = require('express')
+const Joi = require('joi')
+const contactsModel = require('../../model/index')
+
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+const validateNewUser = (req, res, next) => {
+  const { body } = req
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(10).required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().min(7).max(9).required()
+  })
+  const validationResult = schema.validate(body)
+
+  if (validationResult.error) {
+    return res.status(404).json(validationResult.error)
+  }
+
+  next()
+}
+
+const validateUpdateUser = (req, res, next) => {
+  const { body } = req
+  const schema = Joi.object({
+    name: Joi.string().alphanum().min(3).max(10),
+    email: Joi.string().email(),
+    phone: Joi.string().min(7).max(9)
+  })
+  const validationResult = schema.validate(body)
+
+  if (validationResult.error) {
+    return res.status(404).json(validationResult.error)
+  }
+
+  next()
+}
+
+const validateId = async (req, res, next) => {
+  const { contactId } = req.params
+  const contact = await contactsModel.getContactById(contactId)
+  if (!contact) return res.status(404).json({ message: 'User is not found' })
+  next()
+}
+
+router.get('/', async (_req, res) => {
+  const contacts = await contactsModel.listContacts()
+  res.json(contacts)
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:contactId', validateId, async (req, res) => {
+  const { contactId } = req.params
+  const contact = await contactsModel.getContactById(contactId)
+  res.json(contact)
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.post('/', validateNewUser, async (req, res) => {
+  const { body } = req
+  const contact = await contactsModel.addContact(body)
+  res.json(contact)
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.delete('/:contactId', validateId, async (req, res) => {
+  const { contactId } = req.params
+  const contact = await contactsModel.removeContact(contactId)
+  res.json(contact)
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.patch('/:contactId', validateId, validateUpdateUser, async (req, res) => {
+  const { contactId } = req.params
+  const { body } = req
+  const contact = await contactsModel.updateContact(contactId, body)
+  res.json(contact)
 })
 
 module.exports = router
